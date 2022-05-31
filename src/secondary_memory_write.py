1bfbce2ecc7763e055509111e3426162d5df6f46
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2022-05-31 11:00:48 -0500

    <fix> Errors with secondary memory write

--- src/secondary_memory_write.py
+++ src/secondary_memory_write.py
@@ -7,20 +7,21 @@ Description:
 """
 
 
-
 class Block:
-    def __init__(self, term : str, values: []) -> None:
+    def __init__(self, term: str, values: list[int]) -> None:
         self.term = term
         self.values = values
         self.next = -1
+
     def write(self, output_filename):
         file = open(output_filename, "wb")
 
 
 class Hash:
     def __init__(self) -> None:
-        self.dictionary = {"": []}
-        self.sorted = []
+        # self.dictionary = {"": []}
+        self.dictionary = {}
+        self.sorted_hash = []
 
     def add(self, token: str, value: int):
         if token not in self.dictionary:
@@ -35,17 +36,38 @@ class Hash:
             self.dictionary[token].remove(value)
 
     def sorted(self):
-        self.sorted = sorted(self.dictionary)
+        self.sorted_hash = sorted(
+            self.dictionary.items(), key=lambda kv: (kv[1], kv[0]))
 
     def write_block_to_disk(self, output_filename):
-        for (key, values) in self.dictionary.items():
-            block = Block(key=key, values=values)
-            block.write(output_filename=output_filename)
+        file = open(output_filename, "ab")
+        if len(self.sorted_hash) == 0:
+            return
+        for (key, values) in self.sorted_hash:
+            file.write(key.encode())
+            file.write(" ".encode())
+            for i in range(4):
+                if i >= len(values):
+                    file.write(str("0 ").encode())
+                else:
+                    file.write(str(f"{values[i]} ").encode())
+                
+            file.write(b"\n")
+        file.close()
 
-def spimi_invert(inverted_index):
+    def print_unsorted(self):
+        for (key, values) in self.dictionary.items():
+            print(key, values)
+
+    def print_sorted(self):
+        for (key, value) in self.sorted_hash:
+            print(key, value)
+
+
+def spimi_invert(stream_token):
     outputfile = open("newfile.bin", "wb")
     dictionary = Hash()
-    for (token, value) in inverted_index:
+    for (token, value) in stream_token:
         dictionary.add(token=token, value=value)
     dictionary.sorted()
     dictionary.write_block_to_disk()
@@ -54,34 +76,20 @@ def spimi_invert(inverted_index):
 
 if __name__ == "__main__":
     print("Secondary Memory Write")
-
-    L = 5
-    inverted_index = [
-        ("data1.txt", 12),
-        ("data2.txt", 13),
-        ("data3.txt", 14),
-        ("data4.txt", 15),
-        ("data5.txt", 16)
-    ]
-    print(len(inverted_index))
     document = [
-        ("w1id", 1),
-        ("w2id", 2),
-        ("w2id", 1),
+        ("b", 5),
+        ("b", 6),
+        ("b", 7),
+        ("a", 1),
+        ("a", 2),
+        ("a", 3),
+        ("a", 4),
     ]
-    document = [
-        ("W1ID", [1, 3]),
-        ("W2ID", [1, 2, 3]),
-        ("W3ID", [4]),
-        ("W1ID", [1, 3]),
-        ("W2ID", [1, 2, 3]),
-        ("W3ID", [4]),
-        ("W1ID", [1, 3]),
-        ("W2ID", [1, 2, 3]),
-        ("W3ID", [4]),
-        ("W1ID", [1, 3]),
-        ("W2ID", [1, 2, 3]),
-        ("W3ID", [4]),
-    ]
-
-    # write to disk
+    dictionary = Hash()
+    for (token, value) in document:
+        dictionary.add(token=token, value=value)
+    dictionary.print_unsorted()
+    dictionary.sorted()
+    print()
+    dictionary.print_sorted()
+    dictionary.write_block_to_disk("data/information.bin")
